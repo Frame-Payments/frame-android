7ddd646efc5b9261b327b3d97d0b0fa8cdf19525
Eric Townsend <TOWNSENDEB@YAHOO.COM>
2025-04-26 15:42:37 -0500

    Added Charge Intents API calls

---
+++ FrameSDK/src/main/java/com/framepayments/framesdk/chargeintents/ChargeIntentAPI.kt
@@ -0,0 +1,161 @@
+package com.framepayments.framesdk.chargeintents
+
+import com.framepayments.framesdk.FrameNetworking
+
+class ChargeIntentAPI {
+    //MARK: Methods using coroutines
+    suspend fun createChargeIntent(request: ChargeIntentsRequests.CreateChargeIntentRequest): ChargeIntent? {
+        val endpoint = ChargeIntentEndpoints.CreateChargeIntent
+        val (data, _) = FrameNetworking.performDataTaskWithRequest(endpoint, request)
+
+        if (data != null) {
+            return FrameNetworking.parseResponse<ChargeIntent>(data)
+        }
+        return null
+    }
+
+    suspend fun captureChargeIntent(intentId: String, request: ChargeIntentsRequests.CaptureChargeIntentRequest): ChargeIntent? {
+        val endpoint = ChargeIntentEndpoints.CaptureChargeIntent(intentId)
+        val (data, _) = FrameNetworking.performDataTaskWithRequest(endpoint, request)
+
+        if (data != null) {
+            return FrameNetworking.parseResponse<ChargeIntent>(data)
+        }
+        return null
+    }
+
+    suspend fun confirmChargeIntent(intentId: String): ChargeIntent? {
+        val endpoint = ChargeIntentEndpoints.ConfirmChargeIntent(intentId)
+        val (data, _) = FrameNetworking.performDataTask(endpoint)
+
+        if (data != null) {
+            return FrameNetworking.parseResponse<ChargeIntent>(data)
+        }
+        return null
+    }
+
+    suspend fun cancelChargeIntent(intentId: String): ChargeIntent? {
+        val endpoint = ChargeIntentEndpoints.CancelChargeIntent(intentId)
+        val (data, _) = FrameNetworking.performDataTask(endpoint)
+
+        if (data != null) {
+            return FrameNetworking.parseResponse<ChargeIntent>(data)
+        }
+        return null
+    }
+
+    suspend fun getAllChargeIntents(page: Int?, perPage: Int?): List<ChargeIntent>? {
+        val endpoint = ChargeIntentEndpoints.GetAllChargeIntents(page = page, perPage = perPage)
+        val (data, _) = FrameNetworking.performDataTask(endpoint)
+
+        if (data != null) {
+            return FrameNetworking.parseResponse<ChargeIntentResponses.ListChargeIntentsResponse>(data)?.data
+        }
+        return null
+    }
+
+    suspend fun getChargeIntent(intentId: String): ChargeIntent? {
+        val endpoint = ChargeIntentEndpoints.GetChargeIntent(intentId)
+        val (data, _) = FrameNetworking.performDataTask(endpoint)
+
+        if (data != null) {
+            return FrameNetworking.parseResponse<ChargeIntent>(data)
+        }
+        return null
+    }
+
+    suspend fun updateChargeIntent(intentId: String, request: ChargeIntentsRequests.UpdateChargeIntentRequest): ChargeIntent? {
+        val endpoint = ChargeIntentEndpoints.UpdateChargeIntent(intentId)
+        val (data, _) = FrameNetworking.performDataTaskWithRequest(endpoint, request)
+
+        if (data != null) {
+            return FrameNetworking.parseResponse<ChargeIntent>(data)
+        }
+        return null
+    }
+
+    //MARK: Methods using callbacks
+    fun createChargeIntent(request: ChargeIntentsRequests.CreateChargeIntentRequest, completionHandler: (ChargeIntent?) -> Unit) {
+        val endpoint = ChargeIntentEndpoints.CreateChargeIntent
+
+        FrameNetworking.performDataTaskWithRequest(endpoint, request) { data, response, error ->
+            if (data != null) {
+                completionHandler(FrameNetworking.parseResponse<ChargeIntent>(data))
+            } else {
+                completionHandler(null)
+            }
+        }
+    }
+
+    fun captureChargeIntent(intentId: String, request: ChargeIntentsRequests.CaptureChargeIntentRequest, completionHandler: (ChargeIntent?) -> Unit) {
+        val endpoint = ChargeIntentEndpoints.CaptureChargeIntent(intentId)
+
+        FrameNetworking.performDataTask(endpoint) { data, response, error ->
+            if (data != null) {
+                completionHandler(FrameNetworking.parseResponse<ChargeIntent>(data))
+            } else {
+                completionHandler(null)
+            }
+        }
+    }
+
+    fun confirmChargeIntent(intentId: String, completionHandler: (ChargeIntent?) -> Unit) {
+        val endpoint = ChargeIntentEndpoints.ConfirmChargeIntent(intentId)
+
+        FrameNetworking.performDataTask(endpoint) { data, response, error ->
+            if (data != null) {
+                completionHandler(FrameNetworking.parseResponse<ChargeIntent>(data))
+            } else {
+                completionHandler(null)
+            }
+        }
+    }
+
+    fun cancelChargeIntent(intentId: String, completionHandler: (ChargeIntent?) -> Unit) {
+        val endpoint = ChargeIntentEndpoints.CancelChargeIntent(intentId)
+
+        FrameNetworking.performDataTask(endpoint) { data, response, error ->
+            if (data != null) {
+                completionHandler(FrameNetworking.parseResponse<ChargeIntent>(data))
+            } else {
+                completionHandler(null)
+            }
+        }
+    }
+
+    fun getAllChargeIntents(page: Int?, perPage: Int?, completionHandler: (List<ChargeIntent>?) -> Unit) {
+        val endpoint = ChargeIntentEndpoints.GetAllChargeIntents(page = page, perPage = perPage)
+
+        FrameNetworking.performDataTask(endpoint) { data, response, error ->
+            if (data != null) {
+                completionHandler(FrameNetworking.parseResponse<ChargeIntentResponses.ListChargeIntentsResponse>(data)?.data)
+            } else {
+                completionHandler(null)
+            }
+        }
+    }
+
+    fun getChargeIntent(intentId: String, completionHandler: (ChargeIntent?) -> Unit) {
+        val endpoint = ChargeIntentEndpoints.GetChargeIntent(intentId)
+
+        FrameNetworking.performDataTask(endpoint) { data, response, error ->
+            if (data != null) {
+                completionHandler(FrameNetworking.parseResponse<ChargeIntent>(data))
+            } else {
+                completionHandler(null)
+            }
+        }
+    }
+
+    fun updateChargeIntent(intentId: String, request: ChargeIntentsRequests.UpdateChargeIntentRequest, completionHandler: (ChargeIntent?) -> Unit) {
+        val endpoint = ChargeIntentEndpoints.UpdateChargeIntent(intentId)
+
+        FrameNetworking.performDataTaskWithRequest(endpoint, request) { data, response, error ->
+            if (data != null) {
+                completionHandler(FrameNetworking.parseResponse<ChargeIntent>(data))
+            } else {
+                completionHandler(null)
+            }
+        }
+    }
+}
