ec02a5ebb4ab8ee5a543936ef089f57dd27fc3c3
Eric Townsend <TOWNSENDEB@YAHOO.COM>
2025-08-14 02:09:02 -0700

    Update CustomerIdentityRequests.kt

--- FrameSDK/src/main/java/com/framepayments/framesdk/customeridentity/CustomerIdentityRequests.kt
+++ FrameSDK/src/main/java/com/framepayments/framesdk/customeridentity/CustomerIdentityRequests.kt
@@ -5,7 +5,7 @@ import com.framepayments.framesdk.FrameObjects
 
 object CustomerIdentityRequests {
     data class CreateCustomerIdentityRequest(
-        @SerializedName("billing_address") val address: FrameObjects.BillingAddress,
+        @SerializedName("address") val address: FrameObjects.BillingAddress,
         @SerializedName("first_name") val firstName: String,
         @SerializedName("last_name") val lastName: String,
         @SerializedName("date_of_birth") val dateOfBirth: String,
