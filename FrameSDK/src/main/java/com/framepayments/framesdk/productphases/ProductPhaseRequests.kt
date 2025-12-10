9a1d734c3efa0ee493d81b7b2f195075729214ab
Eric Townsend <eric@framepayments.com>
2025-12-10 13:59:19 -0800

    Added nullability to update request

--- FrameSDK/src/main/java/com/framepayments/framesdk/productphases/ProductPhaseRequests.kt
+++ FrameSDK/src/main/java/com/framepayments/framesdk/productphases/ProductPhaseRequests.kt
@@ -15,12 +15,12 @@ object ProductPhaseRequest {
     )
 
     data class UpdateProductPhaseRequest(
-        val ordinal: Int?,
-        val name: String?,
-        @SerializedName("pricing_type") val pricingType: PhasePricingType?,
-        @SerializedName("amount_cents") val amountCents: Int?,
-        @SerializedName("discount_percentage") val discountPercentage: Float?,
-        @SerializedName("period_count") val periodCount: Int?
+        val ordinal: Int? = null,
+        val name: String? = null,
+        @SerializedName("pricing_type") val pricingType: PhasePricingType? = null,
+        @SerializedName("amount_cents") val amountCents: Int? = null,
+        @SerializedName("discount_percentage") val discountPercentage: Float? = null,
+        @SerializedName("period_count") val periodCount: Int? = null
     )
 
     data class BulkUpdateProductPhaseRequest(
