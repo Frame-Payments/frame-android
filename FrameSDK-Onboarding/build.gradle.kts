ae89d5ebda4a50bd1c4b46478aea5b96fb713eb6
Eric Townsend <eric@framepayments.com>
2026-05-12 11:06:53 -0700

    Updated build version

--- FrameSDK-Onboarding/build.gradle.kts
+++ FrameSDK-Onboarding/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
 }
 
 mavenPublishing {
-    coordinates("com.framepayments", "framesdk_onboarding", "2.0.4")
+    coordinates("com.framepayments", "framesdk_onboarding", "2.0.6")
 
     pom {
         name.set("FrameSDK-Onboarding")
